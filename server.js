ab8929670f21647e585037fee8af36b818f5a081
yash0260 <yash54y@gmail.com>
2026-01-04 17:50:32 +0530

    Fix: Add server variable

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
